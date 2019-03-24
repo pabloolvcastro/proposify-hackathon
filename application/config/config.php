efa8b151b33d83c3122d737fbbfdf4892df235a8
Baldavira <stobenetti@gmail.com>
2019-03-24 18:26:51 -0300

    Changing basepath

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/vhck/';
+$config['base_url'] = 'http://localhost/proposify-hackathon/';
 
 /*
 |--------------------------------------------------------------------------
